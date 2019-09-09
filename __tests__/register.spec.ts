c2adf2fe85b117fc6718cb6d4e407bc4532394ba
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-09 17:16:55 -0300

    Getting GraphQL Yoga and Jest to work together - Part 5

--- __tests__/register.spec.ts
+++ __tests__/register.spec.ts
@@ -1,7 +1,7 @@
 import { request } from "graphql-request";
-import { host } from "../src/types/constants";
 import { User } from "../src/entity/User";
-import { createTypeOrmConn } from "../src/utils/createTypesOrmConn";
+import { startServer } from "../src/server";
+import { AddressInfo } from "net";
 
 const email = "toma@bob.com";
 const password = "asdf";
@@ -10,13 +10,16 @@ const mutation = `
     register(email: "${email}", password: "${password}")
   }
 `;
+let getHost = () => "";
 
 beforeAll(async () => {
-  await createTypeOrmConn();
+  const app = await startServer();
+  const { port } = app.address() as AddressInfo;
+  getHost = () => `http://127.0.0.1:${port}`;
 });
 
 test("Register user", async () => {
-  const response = await request(host, mutation);
+  const response = await request(getHost(), mutation);
   expect(response).toEqual({ register: true });
   const users = await User.find({ where: { email } });
   expect(users).toHaveLength(1);
