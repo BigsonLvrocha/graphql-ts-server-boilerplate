c415ad6d62c98dce35a469796733e0eeef606df5
Luiz Victor Linhares Rocha <luiz@sigalei.com.br>
2019-09-11 13:52:10 -0300

    Express Session for Login - Part 15

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,7 @@
 import { GraphQLServer } from "graphql-yoga";
+import * as session from "express-session";
+import * as ConnectRedis from "connect-redis";
+import * as Redis from "redis";
 import { createTypeOrmConn } from "./utils/createTypesOrmConn";
 import { redis } from "./services/redis";
 import { confirmEmail } from "./routes/confirmEmail";
@@ -10,12 +13,34 @@ export const startServer = async () => {
     schema,
     context: ({ request }) => ({
       redis,
-      url: request.protocol + "://" + request.get("host")
+      url: request.protocol + "://" + request.get("host"),
+      session: request.session
     })
   });
+  server.express.use(
+    session({
+      name: "qid",
+      secret: process.env.SESSION_SECRET as string,
+      resave: false,
+      saveUninitialized: false,
+      store: new (ConnectRedis(session))({
+        client: Redis.createClient()
+      }),
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7
+      }
+    })
+  );
+  const cors = {
+    credentials: true,
+    origin: "http://localhost:3000"
+  };
   server.express.get("/confirm/:id", confirmEmail);
   await createTypeOrmConn();
   const app = await server.start({
+    cors,
     port: process.env.NODE_ENV === "test" ? 0 : 4000
   });
   console.log("Server is running on localhost:4000");
